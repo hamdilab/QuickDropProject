547e68978ddf1fd114acb59a3794893264d74ab8
hayet11 <fkirihayet111@gmail.com>
2026-06-28 16:27:10 +0100

    add config server to catalog MS

--- Catalog-service/CatalogService/src/main/java/com/catalogservice/controllers/CategorieController.java
+++ Catalog-service/CatalogService/src/main/java/com/catalogservice/controllers/CategorieController.java
@@ -5,6 +5,7 @@ import com.catalogservice.dtos.CategorieResponse;
 import com.catalogservice.services.CategorieService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,14 @@ import java.util.List;
 public class CategorieController {
 
     private final CategorieService categorieService;
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return welcomeMessage;
+    }
 
     @PostMapping
     public ResponseEntity<CategorieResponse> creer(@Valid @RequestBody CategorieRequest request) {
