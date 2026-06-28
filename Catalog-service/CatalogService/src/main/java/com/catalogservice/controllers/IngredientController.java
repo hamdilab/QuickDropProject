13cbab9b18da751a15d1bde22419998bb7fd4c7e
hayet11 <fkirihayet111@gmail.com>
2026-06-28 12:37:55 +0100

    updates

--- Catalog-service/CatalogService/src/main/java/com/catalogservice/controllers/IngredientController.java
+++ Catalog-service/CatalogService/src/main/java/com/catalogservice/controllers/IngredientController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/ingredients")
+@RequestMapping("/catalog/ingredients")
 @RequiredArgsConstructor
 public class IngredientController {
 
--- Catalog-service/CatalogService/src/main/java/com/catalogservice/controllers/MenuController.java
+++ Catalog-service/CatalogService/src/main/java/com/catalogservice/controllers/MenuController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/menus")
+@RequestMapping("/catalog/menus")
 @RequiredArgsConstructor
 public class MenuController {
 
